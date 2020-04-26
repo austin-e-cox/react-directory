b3fba40d360b2243e0b7b197666d456400813977
Austin Cox <austin.ed.cox@gmail.com>
2020-04-26 00:33:17 -0700

    Updates

---
+++ precache-manifest.06d37a4a11f6d64400e770db963f9f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba970d744bef2e45b2014b236e10580",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "ea587724a109f3cfa3cb",
+    "url": "/gh-pages/static/css/main.0be7b839.chunk.css"
+  },
+  {
+    "revision": "17cdbb93c66ca633e094",
+    "url": "/gh-pages/static/js/2.14f06438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages/static/js/2.14f06438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea587724a109f3cfa3cb",
+    "url": "/gh-pages/static/js/main.cd2c8048.chunk.js"
+  },
+  {
+    "revision": "bae2833e016bff34f906",
+    "url": "/gh-pages/static/js/runtime-main.d33b2c29.js"
+  }
+]);
